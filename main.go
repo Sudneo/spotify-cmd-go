ce2726d7f6f939080e18180a3d734079a88ad9a1
sudneo <mucci.daniele@gmail.com>
2020-08-26 16:14:54 +0300

    Adds flag to select wether album should be printed or not

--- main.go
+++ main.go
@@ -37,6 +37,7 @@ func main() {
 	var prev = flag.Bool("prev", false, "Go to previous song")
 	var prevIcon = flag.Bool("prevIcon", false, "Print the prev icon")
 	var justify = flag.Int("justify", 75, "Justifies the output to the specified number of characters, padding or trimming")
+	var printAlbum = flag.Bool("album", false, "Print also the album information in addition to name and artist")
 	flag.Parse()
 	conn, err := dbus.SessionBus()
 	if err != nil {
@@ -101,11 +102,16 @@ func main() {
 			fmt.Println(err)
 			os.Exit(1)
 		}
+		var status_string string
 		values := metadata.Value()
 		title := values.(map[string]dbus.Variant)["xesam:title"]
 		artist := values.(map[string]dbus.Variant)["xesam:artist"].Value().([]string)[0]
-		album := values.(map[string]dbus.Variant)["xesam:album"]
-		status_string := fmt.Sprintf("%s %s %s (%s)", title, dashUnicode, artist, album)
+		if *printAlbum {
+			album := values.(map[string]dbus.Variant)["xesam:album"]
+			status_string = fmt.Sprintf("%s %s %s (%s)", title, dashUnicode, artist, album)
+		} else {
+			status_string = fmt.Sprintf("%s %s %s", title, dashUnicode, artist)
+		}
 		fmt.Println(trim_or_pad(status_string, *justify))
 		if err = conn.AddMatchSignal(
 			dbus.WithMatchObjectPath("/org/mpris/MediaPlayer2"),
@@ -123,8 +129,12 @@ func main() {
 			values := metadata.Value()
 			title := values.(map[string]dbus.Variant)["xesam:title"]
 			artist := values.(map[string]dbus.Variant)["xesam:artist"].Value().([]string)[0]
-			album := values.(map[string]dbus.Variant)["xesam:album"]
-			status_string := fmt.Sprintf("%s %s %s (%s)", title, dashUnicode, artist, album)
+			if *printAlbum {
+				album := values.(map[string]dbus.Variant)["xesam:album"]
+				status_string = fmt.Sprintf("%s %s %s (%s)", title, dashUnicode, artist, album)
+			} else {
+				status_string = fmt.Sprintf("%s %s %s", title, dashUnicode, artist)
+			}
 			fmt.Println(trim_or_pad(status_string, *justify))
 		}
 	}
